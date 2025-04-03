9759a0b048d8e251c7b84c943493463f1f4334a0
archana333.cse <archana333.cse@gmail.com>
2025-04-03 14:21:13 +0530

    update code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
 }
